68d5f82d083d9cc14a60ba12119e39278bb0f580
˹Ҩ፝֟፝ɴ ꫝɴᴊᴀʟɪ˼ [🇮🇳] ♡゙ <laleshwarraj018@gmail.com>
2024-10-19 19:18:03 +0530

    Update stop.py

--- SUHANIMUSIC/plugins/admins/stop.py
+++ SUHANIMUSIC/plugins/admins/stop.py
@@ -2,7 +2,7 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from SUHANIMUSIC import app
-from SUHANIMUSIC.core.call import PRO
+from SUHANIMUSIC.core.call import SUHANI
 from SUHANIMUSIC.utils.database import set_loop
 from SUHANIMUSIC.utils.decorators import AdminRightsCheck
 from SUHANIMUSIC.utils.inline import close_markup
@@ -16,7 +16,7 @@ from config import BANNED_USERS
 async def stop_music(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
         return
-    await PRO.stop_stream(chat_id)
+    await SUHANI.stop_stream(chat_id)
     await set_loop(chat_id, 0)
     await message.reply_text(
         _["admin_5"].format(message.from_user.mention), reply_markup=close_markup(_)
