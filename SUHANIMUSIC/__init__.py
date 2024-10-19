e19b4d8b14a69097e9cc5e36c5b4804012a5a90b
˹Ҩ፝֟፝ɴ ꫝɴᴊᴀʟɪ˼ [🇮🇳] ♡゙ <laleshwarraj018@gmail.com>
2024-10-19 15:02:22 +0530

    Add files via upload

--- SUHANIMUSIC/__init__.py
+++ SUHANIMUSIC/__init__.py
@@ -1,1 +1,28 @@
+from SUHANIMUSIC.core.bot import PRO
+from SUHANIMUSIC.core.dir import dirr
+from SUHANIMUSIC.core.git import git
+from SUHANIMUSIC.core.userbot import Userbot
+from SUHANIMUSIC.misc import dbb, heroku
 
+from SafoneAPI import SafoneAPI
+from .logging import LOGGER
+
+dirr()
+git()
+dbb()
+heroku()
+
+app = PRO()
+api = SafoneAPI()
+userbot = Userbot()
+
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
