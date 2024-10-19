500364f004eb3d21ce8af7f5c09f1c37e5e2fbe1
˹Ҩ፝֟፝ɴ ꫝɴᴊᴀʟɪ˼ [🇮🇳] ♡゙ <laleshwarraj018@gmail.com>
2024-10-19 19:54:50 +0530

    Update mongochk.py

--- SUHANIMUSIC/plugins/misc/mongochk.py
+++ SUHANIMUSIC/plugins/misc/mongochk.py
@@ -8,7 +8,7 @@ from SUHANIMUSIC import app as SUHANI
 mongo_url_pattern = re.compile(r'mongodb(?:\+srv)?:\/\/[^\s]+')
 
 
-@PRO.on_message(filters.command("mongochk"))
+@SUHANI.on_message(filters.command("mongochk"))
 async def mongo_command(client, message: Message):
     if len(message.command) < 2:
         await message.reply("Please enter your MongoDB URL after the command. Example: /mongochk your_mongodb_url")
