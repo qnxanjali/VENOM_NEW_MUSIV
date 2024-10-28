fd0a86986d42fb9f406458b09f5ec3f3dc32a386
˹Ҩ፝֟፝ɴ ꫝɴᴊᴀʟɪ˼ [🇮🇳] ♡゙ <laleshwarraj018@gmail.com>
2024-10-28 19:34:47 +0530

    Update left.py

--- SUHANIMUSIC/plugins/tools/left.py
+++ SUHANIMUSIC/plugins/tools/left.py
@@ -39,9 +39,9 @@ async def get_userinfo_img(
 
         circular_img = Image.new("RGBA", img.size, (0, 0, 0, 0))
         circular_img.paste(img, (0, 0), mask)
-        resized = circular_img.resize((286, 286))
-        bg.paste(resized, (297, 117), resized)
-
+        resized = circular_img.resize((400, 400))
+        bg.paste(resized, (440, 160), resized)
+        
     img_draw = ImageDraw.Draw(bg)
 
     path = f"./userinfo_img_{user_id}.png"
